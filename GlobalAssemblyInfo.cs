87540d976ac6814aa3b0af446ad7243d61a220b8
Daniel Cazzulino <daniel@cazzulino.com>
2013-09-26 19:13:35 -0300

    Created nuget specs for all supported containers.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("CommonComposition")]
 [assembly: AssemblyProduct("CommonComposition")]
-[assembly: AssemblyCopyright("Copyright 2013, CommonComposition under Apache 2.0 license.")]
+[assembly: AssemblyCopyright("Copyright 2013, ClariusLabs under Apache 2.0 license.")]
 [assembly: AssemblyTrademark("CommonComposition")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.1.1309.2620")]
-[assembly: AssemblyFileVersion("0.1.1309.2620")]
+[assembly: AssemblyVersion("0.1.1309.2622")]
+[assembly: AssemblyFileVersion("0.1.1309.2622")]
 
 // This attribute should be the SemanticVersion one.
-[assembly: AssemblyInformationalVersion("0.1.1309.2620")]
+[assembly: AssemblyInformationalVersion("0.1.1309.2622")]
 
 [assembly: NeutralResourcesLanguage("en")]
 
